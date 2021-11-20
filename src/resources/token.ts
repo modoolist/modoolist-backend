38ed1bf8bff5a5fe31915b62e65dc48dd7a4d042
electrochemistry04 <electrochemistry04@outlook.com>
2021-11-21 00:20:02 +0900

    jwt tokens

--- src/resources/token.ts
+++ src/resources/token.ts
@@ -2,22 +2,22 @@ import jwt from "jsonwebtoken";
 import { User } from "../interfaces";
 import config from "../config";
 import { HttpException } from "../exceptions";
+import { TokenType } from "../types";
 
-export const issueToken = async (identity: User) => {
-  const token = await jwt.sign(
-    {
-      identity,
-    },
-    config.jwtSecret as string,
-    {
-      algorithm: "HS512",
-      expiresIn: "1w",
-    }
-  );
-  return token;
+export const getTokenType = async (token: string): Promise<TokenType> => {
+  try {
+    const payload: any = await jwt.verify(token, config.jwtSecret as string);
+    return payload.refresh ? "REFRESH" : "ACCESS";
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      throw new HttpException(401, "토큰이 만료되었습니다.");
+    } else if (["jwt malformed", "invalid signature"].includes(error.message)) {
+      throw new HttpException(401, "토큰이 변조되었습니다.");
+    } else throw new HttpException(401, "토큰에 문제가 있습니다.");
+  }
 };
 
-export const veriToken = async (token: string): Promise<User> => {
+export const verify = async (token: string) => {
   try {
     const { identity }: any = await jwt.verify(
       token,
@@ -31,4 +31,39 @@ export const veriToken = async (token: string): Promise<User> => {
       throw new HttpException(401, "토큰이 변조되었습니다.");
     } else throw new HttpException(401, "토큰에 문제가 있습니다.");
   }
+};
+
+export const issue = async (
+  identity: User,
+  refresh: boolean,
+  expiresIn?: string
+) => {
+  if (refresh) {
+    const token = await jwt.sign(
+      {
+        identity: {
+          idx: identity.id,
+        },
+        refresh: true,
+      },
+      config.jwtSecret as string,
+      {
+        algorithm: "HS512",
+        expiresIn: "1y",
+      }
+    );
+    return token;
+  }
+  const token = await jwt.sign(
+    {
+      identity,
+      refresh: false,
+    },
+    config.jwtSecret as string,
+    {
+      algorithm: "HS512",
+      expiresIn: expiresIn ? expiresIn : "1w",
+    }
+  );
+  return token;
 };
